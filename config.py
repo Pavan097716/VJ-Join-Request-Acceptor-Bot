2ed320460c13f54cc4e4a53281572e33f72cfae3
Pavan7130 <pjyijij@gmail.com>
2025-03-02 03:58:57 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,15 +1,15 @@
 from os import environ
 
-API_ID = int(environ.get("API_ID", ""))
-API_HASH = environ.get("API_HASH", "")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
+API_ID = int(environ.get("API_ID", "20793620"))
+API_HASH = environ.get("API_HASH", "a712d2b8486f26c4dee5127cc9ae0615")
+BOT_TOKEN = environ.get("BOT_TOKEN", "7966013957:AAFOzG-__JKwT-uhiLihKPlOJ28-3AueL98")
 
 # Make Bot Admin In Log Channel With Full Rights
-LOG_CHANNEL = int(environ.get("LOG_CHANNEL", ""))
-ADMINS = int(environ.get("ADMINS", ""))
+LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "1002296271857"))
+ADMINS = int(environ.get("ADMINS", "6853851676"))
 
 # Warning - Give Db uri in deploy server environment variable, don't give in repo.
-DB_URI = environ.get("DB_URI", "") # Warning - Give Db uri in deploy server environment variable, don't give in repo.
+DB_URI = environ.get("DB_URI", "mongodb+srv://pokemonchannel098:yaE7BvFwWIXdb3HQ@cluster0.gdr57.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0") # Warning - Give Db uri in deploy server environment variable, don't give in repo.
 DB_NAME = environ.get("DB_NAME", "vjjoinrequetbot")
 
 # If this is True Then Bot Accept New Join Request 
